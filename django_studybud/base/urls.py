6284829b920b6aa77585ecb2d68e5a3de62b293b
Ali ;) <cliali1181@gmail.com>
2024-11-25 22:58:43 +0330

    💄 some styling

--- django_studybud/base/urls.py
+++ django_studybud/base/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "base"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("room", views.room, name="room"),
+    path("room/<int:pk>/", views.room, name="room"),
     path("create-room/", views.create_room, name="create-room"),
 ]
