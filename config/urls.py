554ee2eb2936e61b3e14aaaf2866bb0397966625
Ali ;) <cliali1181@gmail.com>
2024-12-09 23:55:05 +0330

    feat(media): add MEDIA_ROOT, MEDIA_URL

--- config/urls.py
+++ config/urls.py
@@ -17,4 +17,8 @@ urlpatterns = [
     path("core/", include(("django_studybud.core.urls", "core"))),
     path("base/", include(("django_studybud.base.urls", "bsae"))),
     path("accounts/", include("allauth.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
